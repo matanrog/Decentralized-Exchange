1b3175d3c100979c8fda5d19e1ac595a0e250a93
Matan Roginsky <matanrog@gmail.com>
2023-03-07 23:08:38 +0200

    7. Token Transfer Delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -36,13 +36,19 @@ contract Token {
         uint256 _value
     ) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
+
+        _transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal {
         require(_to != address(0));
 
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        balanceOf[_from] = balanceOf[_from] - _value;
         balanceOf[_to] = balanceOf[_to] + _value;
 
-        emit Transfer(msg.sender, _to, _value);
-        return true;
+        emit Transfer(_from, _to, _value);
     }
 
     function approve(
@@ -53,6 +59,21 @@ contract Token {
         allowance[msg.sender][_spender] = _value;
 
         emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint _value
+    ) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+        _transfer(_from, _to, _value);
+
         return true;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -126,4 +126,49 @@ describe('Token', () => {
             })
         })
     })
+
+    describe('Delegated Token Transfers', () => { 
+
+        let amount, transaction, result;
+
+            beforeEach(async () => {
+                amount = tokenSup(100);
+                transaction = await token.connect(deployer).approve(exchange.address, amount);
+                result = await transaction.wait();
+            })
+
+        describe('Success', () => { 
+            beforeEach(async () => {
+                transaction = await token.connect(exchange).transferFrom(deployer.address, receiver.address, amount);
+                result = await transaction.wait();
+            })
+
+            it('transfers token balances', async () => {
+                expect(await token.balanceOf(deployer.address)).to.equal(ethers.utils.parseUnits('999900','ether'));
+                expect(await token.balanceOf(receiver.address)).to.equal(amount);
+            })
+
+            it('resets the allowance', async () => {
+                expect(await token.allowance(deployer.address, exchange.address)).to.be.equal(0);
+            })
+
+            it('emits an Transfer event', async () =>{
+                const event = result.events[0];
+                expect(event.event).to.equal('Transfer');
+
+                const args = event.args;
+                expect(args.from).to.equal(deployer.address);
+                expect(args.to).to.equal(receiver.address);
+                expect(args.value).to.equal(amount);
+            })
+         })
+
+        describe('Failure', () => { 
+            it('Rejects insufficient amounts', async () => {
+		        const invalidAmount = tokenSup(100000000)
+		        await expect(token.connect(exchange).transferFrom(deployer.address, receiver.address, invalidAmount)).to.be.reverted;
+	        })
+         })
+        
+     })
 })
