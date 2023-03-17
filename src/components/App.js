8362bd0740bdbd5c915903a824d954bdbce72421
Matan Roginsky <matanrog@gmail.com>
2023-03-17 14:44:12 +0200

    26. Trades

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,7 @@ import Balance from './Balance'
 import Order from './Order';
 import OrderBook from './OrderBook';
 import PriceChart from './PriceChart';
+import Trades from './Trades';
 
 function App() {
   const dispatch = useDispatch()
@@ -81,7 +82,7 @@ function App() {
 
           {/* Transactions */}
 
-          {/* Trades */}
+          <Trades />
 
           <OrderBook />
 
