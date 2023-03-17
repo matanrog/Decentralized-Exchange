854f784dcaf2aef9a8120466937736e8a400c30f
Matan Roginsky <matanrog@gmail.com>
2023-03-17 14:19:42 +0200

    25. PriceChart

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,7 @@ import Markets from './Markets'
 import Balance from './Balance'
 import Order from './Order';
 import OrderBook from './OrderBook';
+import PriceChart from './PriceChart';
 
 function App() {
   const dispatch = useDispatch()
@@ -76,7 +77,7 @@ function App() {
         </section>
         <section className='exchange__section--right grid'>
 
-          {/* PriceChart */}
+          <PriceChart />
 
           {/* Transactions */}
 
