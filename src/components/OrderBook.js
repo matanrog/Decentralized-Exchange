d69a6e2c347b2922a5013b2e43d891a6fe334fc4
Matan Roginsky <matanrog@gmail.com>
2023-03-18 10:20:55 +0200

    30. Filling Orders

--- src/components/OrderBook.js
+++ src/components/OrderBook.js
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 // Import Assets
 import sort from '../assets/sort.svg'
@@ -6,9 +6,20 @@ import sort from '../assets/sort.svg'
 // Import Selectors
 import { orderBookSelector } from '../store/selectors'
 
+// Import Interactions
+import { fillOrder } from '../store/interactions';
+
 const OrderBook = () => {
+  const provider = useSelector(state => state.provider.connection)
+  const exchange = useSelector(state => state.exchange.contract)
   const symbols = useSelector(state => state.tokens.symbols)
   const orderBook = useSelector(orderBookSelector)
+
+  const dispatch = useDispatch()
+
+  const fillOrderHandler = (order) => {
+    fillOrder(provider, exchange, order, dispatch)
+  }
 
   return (
     <div className="component exchange__orderbook">
@@ -36,7 +47,7 @@ const OrderBook = () => {
 
               {orderBook && orderBook.sellOrders.map((order, index) => {
                 return(
-                <tr key={index}>
+                <tr key={index} onClick={() => fillOrderHandler(order)}>
                   <td>{order.token0Amount}</td>
                   <td style={{ color: `${order.orderTypeClass}` }}>{order.tokenPrice}</td>
                   <td>{order.token1Amount}</td>
@@ -68,7 +79,7 @@ const OrderBook = () => {
 
               {orderBook && orderBook.buyOrders.map((order, index) => {
                 return (
-                  <tr key={index}>
+                  <tr key={index} onClick={() => fillOrderHandler(order)}>
                     <td>{order.token0Amount}</td>
                     <td style={{ color: `${order.orderTypeClass}` }}>{order.tokenPrice}</td>
                     <td>{order.token1Amount}</td>
